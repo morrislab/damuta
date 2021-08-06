b7d0e71e17ba0a5e32e676884677bab283849925
harrig12 <cait.harrigan@mail.utoronto.ca>
2021-08-06 13:23:09 -0400

    parameterization improved for wandb sweeps

---
+++ run.py
@@ -0,0 +1,33 @@
+import wandb
+import damut as da
+from damut.utils import load_config, split_data
+import argparse
+
+if __name__ == "__main__":
+
+    # pick up config file path
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', help='yaml config file', default = "config/config-defaults.yaml",
+                        '-p', help='wandb project name to assign run to', default = 'foo'
+                       )
+    args = parser.parse_args()
+
+    # load config
+    dataset_args, model_args, pymc3_args = load_config(args.f)
+
+    # init run
+    run = wandb.init()
+    run.config.update({**dataset_args, **model_args, **pymc3_args})
+
+    # load data 
+    counts = da.load_dataset(dataset_args)
+    trn, val, tst1, tst2 = split_data(counts)
+    
+    # fit model for train samples
+    model, trace = da.infer(trn.to_numpy(), val.to_numpy(), model_args, pymc3_args)
+
+    # evaluate val
+
+    # fit model for test samples
+
+    # evaluate tst2
--- sweep_runner.py
+++ sweep_runner.py
@@ -1,15 +1,24 @@
 import wandb
-import os
-import damut
-​
+import damut as da
+from damut.utils import load_config
+
 if __name__ == "__main__":
-​
-	defaults_fp = "config/config-defaults.yml"
 
-	# init run
-	run = wandb.init()
-	# configure with defaults and sweep args
-	conf = damut.load_sweep_config(run, defaults_fp)
+    dataset_args, model_args, pymc3_args = load_config("config/config-defaults.yaml")
+    
 
-	# fit model
-	damut.infer(conf)
+    # init run
+    run = wandb.init()
+    wandb.log({'destargs': dataset_args})
+    # configure run with defaults and sweep args
+    run.config.setdefaults({**dataset_args, **model_args, **pymc3_args})
+    
+    # override config with any arguments set up in wandb run
+    # (useful for managing wandb sweeps) Adamo Young 2021
+    run_config_d = dict(run.config)
+    for d in [dataset_args, model_args, pymc3_args]:
+        for k,v in d.items():
+            d[k] = run_config_d[k]
+
+    # fit model
+    da.infer(10, model_args, pymc3_args)
