70f5601d5a04950799ddd00c7070f8d2be5eec92
harrig12 <cait.harrigan@mail.utoronto.ca>
2022-03-28 12:01:49 -0400

    model debugs

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,12 +8,12 @@ def test_data():
     return pl.Path(__file__).resolve().parent / 'test_data'
 
 @pytest.fixture(scope="session")
-def pcawg():
+def pcawg(test_data):
     counts = pd.read_csv(test_data / 'pcawg_counts.csv',  index_col=0)
     annotation = pd.read_csv(test_data / 'pcawg_cancer_types.csv', index_col=0)
     return DataSet(counts, annotation)
 
 @pytest.fixture(scope="session")
-def cosmic():
+def cosmic(test_data):
     sigs = pd.read_csv(test_data / 'COSMIC_v3.2_SBS_GRCh37.txt', sep='\t', index_col = 0).T
     return SignatureSet(sigs)
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,4 @@
-import pandas as pd
-from damuta.models import Lda
+from damuta.models import Lda, TandemLda, HierarchicalTendemLda
 
 def test_DataSet_init(pcawg):
     assert pcawg.nsamples == 2778
@@ -8,10 +7,23 @@ def test_SignatureSet_init(cosmic):
     assert cosmic.nsigs == 78
     assert cosmic.damage_signatures.shape == (78,32)
     assert cosmic.misrepair_signatures.shape == (78,6)
-    assert cosmic.summarize_separation.shape == (8,3)
+    assert cosmic.summarize_separation().shape == (8,3)
     
-def test_Lda_init(pcawg):
+def test_Lda(pcawg):
     
-    model = Lda(pcawg)
+    model = Lda(dataset=pcawg, n_sigs=10)
+    assert model.dataset.counts.shape == pcawg.counts.shape
+    model.fit(15)
+
+def test_TandemLda(pcawg):
     
+    model = TandemLda(dataset=pcawg, n_damage_sigs=10, n_misrepair_sigs=5)
+    assert model.dataset.counts.shape == pcawg.counts.shape
+    model.fit(15)
     
+def test_HierarchicalTandemLda(pcawg):
+    
+    model = HierarchicalTendemLda(dataset=pcawg, n_damage_sigs=10, 
+                                  n_misrepair_sigs=5, type_col='tissue_type')
+    assert model.dataset.counts.shape == pcawg.counts.shape
+    model.fit(15)
