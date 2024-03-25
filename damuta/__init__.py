2b4a61fcbe6543b9cd987f52d91127fe9ee52062
Cait Harrigan <harrigc@crick.ac.uk>
2024-03-25 06:34:23 -0400

    hotfix module import

--- damuta/__init__.py
+++ damuta/__init__.py
@@ -8,7 +8,7 @@ The most commonly used functions/objects are:
 """
 
 from .base import DataSet, SignatureSet
-from damuta import models, plotting, callbacks
+from . import models, plotting, callbacks
 
 __version__ = "0.1.1"
 
